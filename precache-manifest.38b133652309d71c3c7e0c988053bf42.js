8377153816d69a87d20bea48881ee7721a3543f7
Clément Garin <clement.garin@outlook.fr>
2023-06-06 11:48:43 +0200

    Updates

---
+++ precache-manifest.38b133652309d71c3c7e0c988053bf42.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99914e17ea67267ecb001ddc12f00fff",
+    "url": "/vanta-scooting-tool/index.html"
+  },
+  {
+    "revision": "e177d361ba6a51ded34f",
+    "url": "/vanta-scooting-tool/static/css/2.82ceb22c.chunk.css"
+  },
+  {
+    "revision": "ba67bae8de187e56aa3f",
+    "url": "/vanta-scooting-tool/static/css/main.8790ea2c.chunk.css"
+  },
+  {
+    "revision": "e177d361ba6a51ded34f",
+    "url": "/vanta-scooting-tool/static/js/2.031abda5.chunk.js"
+  },
+  {
+    "revision": "462f991c1d2ef16102b314f225132ac4",
+    "url": "/vanta-scooting-tool/static/js/2.031abda5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba67bae8de187e56aa3f",
+    "url": "/vanta-scooting-tool/static/js/main.fec4d697.chunk.js"
+  },
+  {
+    "revision": "316bb5515e98d1b389db",
+    "url": "/vanta-scooting-tool/static/js/runtime-main.72d86b41.js"
+  },
+  {
+    "revision": "37d0af67f43b1b9293101869f19bdec5",
+    "url": "/vanta-scooting-tool/static/media/report_ref.37d0af67.jpg"
+  },
+  {
+    "revision": "1e1eb54cc23c5b980a86ca7036b359c2",
+    "url": "/vanta-scooting-tool/static/media/report_ref_concentration.1e1eb54c.jpg"
+  },
+  {
+    "revision": "90305e597445ab7912551e858b9b110d",
+    "url": "/vanta-scooting-tool/static/media/report_value.90305e59.jpg"
+  }
+]);
